9071036c0d07bb2319ee191518aa57904df8355c
ttdantett <ttdantett@gmail.com>
2019-08-23 20:49:48 +0200

    19/08/2019

--- polybius_square/README.py
+++ polybius_square/README.py
@@ -1,4 +1,14 @@
 # Polybius square cipher 
+
+| - | 1 | 2 | 3 | 4 | 5
+| --- | --- | --- | --- | --- | --- |
+| *1* | A | B | C | D | E |
+| *2* | F | G | H | I | J |
+| *3* | K | L | M | N | O |
+| *4* | P | Q | R | S | T |
+| *5* | U | V | W | X | Y |
+
+
 #Usage example:
 cipher = encrypt("Thisisatest")
 print(cipher)
